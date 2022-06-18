ada94548051a6e0bdf6620d9976e8fe8f3b15bde
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-06-19 03:48:13 +0400

    Intro done 80%

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col } from "reactstrap";
 import Navbar from "./Components/Navbar/Navbar";
 import "./assets/css/style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
@@ -22,7 +24,20 @@ function App() {
             <Row>
               <Col lg="12" className="Items-intro">
                 <Row>
-                  <Col lg="6 border"></Col>
+                  <Col lg="6 border" className="left-col">
+                    <h1>Better Solutions For Your Business</h1>
+                    <h2>
+                      We are team of talented designers making websites with
+                      Bootstrap
+                    </h2>
+                    <div className="d-flex justify-content-center justify-content-lg-start">
+                      <a className="btn btn-primary myButton">Get Started</a>
+                      <a className="video-my">
+                        <FontAwesomeIcon icon={faCirclePlay} className="logo"></FontAwesomeIcon>
+                        <span>Watch Video</span>
+                      </a>
+                    </div>
+                  </Col>
                   <Col lg="6 border">
                     <div className="d-flex">
                       <img
