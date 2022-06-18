1b6c273087be6f64eb79dc5b5519900515a9612e
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-06-19 00:21:24 +0400

    Created Intro Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Container, Row } from "reactstrap";
-import Navbar from "./Components/Navbar/Navbar"
+import { Container, Row, Col } from "reactstrap";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Container>
-      <Row>
-        <Navbar />
-      </Row>
+        <Row>
+          <Navbar />
+        </Row>
+      </Container>
+      <Container fluid className="mt-5">
+        <Row>
+          <Col lg="12">
+            
+          </Col>
+        </Row>
       </Container>
     </div>
   );
