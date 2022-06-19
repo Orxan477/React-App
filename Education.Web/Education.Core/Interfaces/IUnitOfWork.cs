f89c85357cda8eeb701906e0f20b9a3ca369ba18
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-06-20 01:09:14 +0400

    IUnitOfWork done

---
+++ Education.Web/Education.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Education.Core.Interfaces.Employee;
+
+namespace Education.Core.Interfaces
+{
+    internal interface IUnitOfWork
+    {
+        public IEmployeeRepository EmployeeRepository { get; }
+        Task SaveChangeAsync();
+    }
+}
