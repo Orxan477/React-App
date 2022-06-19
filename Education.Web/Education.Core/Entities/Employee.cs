f620ea2d34769bbd0b265eaa4c09d80a3063b841
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-06-20 00:15:02 +0400

    Added readme file and created entities

---
+++ Education.Web/Education.Core/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Education.Core.Entities
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ Education.Web/Education.Core/Entities/Intro.cs
@@ -0,0 +1,9 @@
+﻿namespace Education.Core.Entities
+{
+    internal class Intro
+    {
+        public int Id { get; set; }
+        public string Head { get; set; }
+        public string Content { get; set; }
+    }
+}
