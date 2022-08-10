18f260748b13fd7c11ae470b80b6e2d28bbd0b4c
Orxan477 <tu201906147@code.edu.az>
2022-08-10 17:15:59 +0400

    LoginVM Validator done

---
+++ Education.Web/Education.Business/Validations/Account/LoginVMValidation.cs
@@ -0,0 +1,14 @@
+﻿using Education.Business.ViewModels.Account;
+using FluentValidation;
+
+namespace Education.Business.Validations.Account
+{
+    public class LoginVMValidation:AbstractValidator<LoginVM>
+    {
+        public LoginVMValidation()
+        {
+            RuleFor(x => x.Email).NotEmpty().NotNull().MaximumLength(255);
+            RuleFor(x => x.Password).NotEmpty().NotNull().MaximumLength(255);
+        }
+    }
+}
--- Education.Web/Education.Web/Controllers/AccountController.cs
+++ Education.Web/Education.Web/Controllers/AccountController.cs
@@ -65,6 +65,7 @@ namespace Education.Web.Controllers
         [Route("login")]
         public async Task<string> Login(LoginVM login)
         {
+            // Validation yarat 
             return "Hele Hazirlanir";
         }
     }
