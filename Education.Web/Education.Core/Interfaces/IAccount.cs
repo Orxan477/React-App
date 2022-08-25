cd5a438d0262c3f297e48f22e3cb34db7581fec5
Orxan477 <tu201906147@code.edu.az>
2022-08-25 15:17:32 +0400

    Created IAccount Repository Interfacce

---
+++ Education.Web/Education.Core/Interfaces/IAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Education.Core.Interfaces
+{
+    public interface IAccount<TEntity>
+    {
+        Task Register(TEntity entity);
+        Task<bool> Login(TEntity entity);
+        Task<string> CreateRole();
+    }
+}
